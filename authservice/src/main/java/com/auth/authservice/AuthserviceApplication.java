956b7760b144977cddd0ba3399565270500b6026
Deck52 <bharath.v.2405@gmail.com>
2026-05-02 12:54:52 +0530

    Added PostgreSQL connection and user entity

--- authservice/src/main/java/com/auth/authservice/AuthserviceApplication.java
+++ authservice/src/main/java/com/auth/authservice/AuthserviceApplication.java
@@ -2,15 +2,9 @@ package com.auth.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(
-    exclude = {
-        DataSourceAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
-    }
-)
+
+@SpringBootApplication
 public class AuthserviceApplication {
 
     public static void main(String[] args) {
