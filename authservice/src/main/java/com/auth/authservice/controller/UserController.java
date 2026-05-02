4cf655c7189d7be43a981a352c6130551a8e2d2c
Deck52 <bharath.v.2405@gmail.com>
2026-05-02 13:17:10 +0530

    Backend write operation

---
+++ authservice/src/main/java/com/auth/authservice/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.auth.authservice.controller;
+
+import com.auth.authservice.entity.User;
+import com.auth.authservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+}
---
+++ authservice/src/main/java/com/auth/authservice/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.auth.authservice.repository;
+
+import com.auth.authservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
